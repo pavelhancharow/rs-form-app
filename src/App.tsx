0dc7f0877b2046672707e5dfd0d181ebe129b520
Hancharou <pl.hancharou@gmail.com>
2025-03-11 20:24:35 +0100

    refactor: implement RootLayout and Main page to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { lazy, Suspense } from 'react';
+import { Navigate, Route, Routes } from 'react-router';
+import Loader from './components/Loader/Loader.tsx';
+import RootLayout from './components/RootLayout/RootLayout.tsx';
+
+const MainPage = lazy(() => import('./pages/MainPage.tsx'));
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/forms" />} />
+
+        <Route path="forms" element={<RootLayout />}>
+          <Route index element={<MainPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,5 @@
+function MainPage() {
+  return <div>Forms List</div>;
+}
+
+export default MainPage;
